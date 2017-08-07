8111a05968a14b9a9876b81cb24607dbf1f7b2da
Grigoriy Laptev <wipmoon@gmail.com>
2017-08-08 03:19:44 +0700

    добавил практику 17 и 18

---
+++ Lesson_18.1/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.popup-btn').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeIn();
+  });
+  $('.popup-close').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').fadeOut();
+  });
+  $('.features-slider').slick({
+	  infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        prevArrow: '<button class="prev arrow"></button>',
+        nextArrow: '<button class="next arrow"></button>',
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        prevArrow: '<button class="prev arrow"></button>',
+        nextArrow: '<button class="next arrow"></button>',
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
